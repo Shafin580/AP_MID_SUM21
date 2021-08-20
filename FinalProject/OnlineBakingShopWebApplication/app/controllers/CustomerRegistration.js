85d996cc37eefefad3006568cc4cc1305d9f3b2a
Shafin580 <shafinwork580@gmail.com>
2021-08-20 15:19:32 +0600

    Login & Registration complete validation

--- FinalProject/OnlineBakingShopWebApplication/app/controllers/CustomerRegistration.js
+++ FinalProject/OnlineBakingShopWebApplication/app/controllers/CustomerRegistration.js
@@ -1,49 +1,103 @@
 app.controller("CustomerRegistration",function($scope,$http,ajax,$location){
-    
+    var hasError;
+    function nameValidation(name){
+        if(name===""){
+            $scope.nameError = "Name can't be empty";
+            hasError = true;
+        }
+        else if(String(name).split(" ").length < 2){
+            $scope.nameError = "Must contain atleast 2 words!";
+            hasError = true;
+        }
+    }
+    function usernameValidation(username){
+        if(username===""){
+            $scope.usernameError = "Username can't be empty";
+            hasError = true;
+        }
+        else if(String(username).split(" ").length > 1){
+            $scope.usernameError = "Can not contain space!";
+            hasError = true;
+        }
+    }
+    function passwordValidation(password){
+        if(password===""){
+            $scope.passwordError = "Password can't be empty";
+            hasError = true;
+        }
+        else if(String(password).split(" ").length > 1){
+            $scope.passwordError = "Can not contain space!";
+            hasError = true;
+        }
+    }
+    function emailValidation(email){
+        if(email===""){
+            $scope.emailError = "Email can't be empty";
+            hasError = true;
+        }
+        else if(String(email).split(" ").length > 1){
+            $scope.emailError = "Invalid email!";
+            hasError = true;
+        }
+        else if(String(email).includes("@")==false){
+            $scope.emailError = "Invalid email!";
+            hasError = true;
+        }
+    }
     $scope.register = function(){
-        var customerdata = {
-            name: $scope.name,
-            Username: $scope.username,
-            Email: $scope.email,
-            Password: $scope.password
-        };
+        $scope.nameError = "";
+        $scope.usernameError = "";
+        $scope.passwordError = "";
+        $scope.emailError = "";
+        hasError = false;
+        nameValidation($scope.name);
+        usernameValidation($scope.username);
+        emailValidation($scope.email);
+        passwordValidation($scope.password);
 
-        var logindata = {
-            LoginUsername: $scope.username,
-            LoginEmail: $scope.email,
-            LoginPassword: $scope.password,
-            UserType: "Customer"
-        };
-
-        ajax.get(API_ROOT+"api/Login/"+$scope.username,success,error);
-        function success(response){
-            if(response.data == null){
-                ajax.post(API_ROOT+"api/Login/Add",logindata,
-            function(response){
+        if(!hasError){
+            var customerdata = {
+                name: $scope.name,
+                Username: $scope.username,
+                Email: $scope.email,
+                Password: $scope.password
+            };
+    
+            var logindata = {
+                LoginUsername: $scope.username,
+                LoginEmail: $scope.email,
+                LoginPassword: $scope.password,
+                UserType: "Customer"
+            };
+    
+            ajax.get(API_ROOT+"api/Login/"+$scope.username,success,error);
+            function success(response){
+                if(response.data == null){
+                    ajax.post(API_ROOT+"api/Login/Add",logindata,
+                function(response){
+                    
+                    ajax.post(API_ROOT+"api/Customer/Add",customerdata,
+                function(response){
+                    $scope.successMsg = "Registration was successful!";
+                }
+                ,function(error){
+                    $scope.errorMsg = "Email already exist!";
+                });
+    
+                }
+                ,function(error){
+            
+                });
+                }else{
+                    $scope.errorMsg = "User already exist";
+                }
                 
-                ajax.post(API_ROOT+"api/Customer/Add",customerdata,
-            function(response){
-                $scope.successMsg = "Registration was successful!";
             }
-            ,function(error){
-                $scope.errorMsg = "Email already exist!";
-            });
-
+            function error(err){
+    
+                
             }
-            ,function(error){
-        
-            });
-            }else{
-                $scope.errorMsg = "User already exist";
-            }
-            
         }
-        function error(err){
-
-            
-        }
-
-
 
     }
 
--- FinalProject/OnlineBakingShopWebApplication/app/controllers/Login.js
+++ FinalProject/OnlineBakingShopWebApplication/app/controllers/Login.js
@@ -1,44 +1,53 @@
-app.controller("Login",function($scope,$http,ajax,$location){
-    $scope.login = function(){
-        ajax.get(API_ROOT+"api/Login/"+$scope.username,success,error);
-        function success(response){
-            user = response.data;
-            if(user.LoginUsername == $scope.username && user.LoginPassword == $scope.password){
-                if(user.UserType == "Customer"){
-                    ajax.get(API_ROOT+"api/Customer/"+$scope.username,a,b);
-                    function a(re){
-                        user = re.data;
-                    }
-                    function b(e){
-
-                    }
-                    $location.path("/CustomerHome");
-                }
-                else if(user.UserType == "Courier"){
-                    ajax.get(API_ROOT+"api/Courier/"+$scope.username,c,d);
-                    function c(res){
-                        user = res.data;
-                    }
-                    function d(e){
-                        
-                    }
-                    $location.path("/CourierHome");
-                }
-                else if(user.UserType == "Admin"){
-                    ajax.get(API_ROOT+"api/Admin/"+$scope.username,e,f);
-                    function e(respo){
-                        user = respo.data;
-                    }
-                    function f(erro){
-                        
-                    }
-                    $location.path("/AdminHome");
-                }
-                
-            }
-        }
-        function error(err){
-            $location.path("/Login");
-        }
+app.controller("Login", function ($scope, $http, ajax, $location) {
+  $scope.login = function () {
+    $scope.usernameError = "";
+    $scope.passwordError = "";
+    var hasError = false;
+    function validation() {
+      if ($scope.username === "") {
+        $scope.usernameError = "Username can't be empty!";
+        hasError = true;
+      } else if ($scope.password === "") {
+        $scope.passwordError = "Password can't be empty!";
+        hasError = true;
+      }
     }
+    validation();
+    if (!hasError) {
+      ajax.get(API_ROOT + "api/Login/" + $scope.username, success, error);
+      function success(response) {
+        user = response.data;
+        if (
+          user.LoginUsername == $scope.username &&
+          user.LoginPassword == $scope.password
+        ) {
+          if (user.UserType == "Customer") {
+            ajax.get(API_ROOT + "api/Customer/" + $scope.username, a, b);
+            function a(re) {
+              user = re.data;
+            }
+            function b(e) {}
+            $location.path("/CustomerHome");
+          } else if (user.UserType == "Courier") {
+            ajax.get(API_ROOT + "api/Courier/" + $scope.username, c, d);
+            function c(res) {
+              user = res.data;
+            }
+            function d(e) {}
+            $location.path("/CourierHome");
+          } else if (user.UserType == "Admin") {
+            ajax.get(API_ROOT + "api/Admin/" + $scope.username, e, f);
+            function e(respo) {
+              user = respo.data;
+            }
+            function f(erro) {}
+            $location.path("/AdminHome");
+          }
+        }
+      }
+      function error(err) {
+        $location.path("/Login");
+      }
+    }
+  };
 });
